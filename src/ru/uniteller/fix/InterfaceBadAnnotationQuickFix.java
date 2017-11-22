da0ce423ee8c56b0515b7a8c24bf4faaca478c4b
Alexander <gasfull98@gmail.com>
2017-11-22 14:52:16 +0300

    Created Quick Fix class to Interface

--- src/ru/uniteller/fix/InterfaceBadAnnotationQuickFix.java
+++ src/ru/uniteller/fix/InterfaceBadAnnotationQuickFix.java
@@ -1,4 +1,58 @@
 package ru.uniteller.fix;
 
-public class InterfaceBadAnnotationQuickFix {
+import com.intellij.codeInsight.hint.HintManager;
+import com.intellij.codeInspection.LocalQuickFixAndIntentionActionOnPsiElement;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.jetbrains.php.lang.psi.elements.PhpClass;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import ru.uniteller.PhpClassAndMethod;
+
+import java.util.Map;
+
+public class InterfaceBadAnnotationQuickFix extends LocalQuickFixAndIntentionActionOnPsiElement {
+    private Map<String, PhpClassAndMethod> missingMethod;
+    private PhpClass phpClass;
+
+    public InterfaceBadAnnotationQuickFix(@NotNull PhpClass phpClass, Map<String, PhpClassAndMethod> methodMap) {
+        super(phpClass);
+        this.phpClass = phpClass;
+        this.missingMethod = methodMap;
+    }
+
+    @Override
+    public void invoke(@NotNull Project project, @NotNull PsiFile file, @Nullable Editor editor, @NotNull PsiElement startElement, @NotNull PsiElement endElement) {
+        if (editor == null) return;
+        if (phpClass.getDocComment() == null)
+            return;
+        if (phpClass.getDocComment().getMethods().length == 0) {
+            HintManager.getInstance().showErrorHint(editor, "Ops, nothing found");
+            return;
+        }
+        //TODO
+    }
+
+
+    private void invoke() {
+
+
+    }
+
+    @NotNull
+    @Override
+    public String getText() {
+        return "Добавить методы (" + missingMethod.size() + ")";
+    }
+
+    @Nls
+    @NotNull
+    @Override
+    public String getFamilyName() {
+        return "Добавить";
+    }
+
 }
